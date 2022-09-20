b703341b79f012e8482da14c3540482a8258e477
Will <wrightw404@gmail.com>
2022-09-20 17:58:26 -0400

    initial commit, added questions to the array in index.js, added a gitignore file for node_modules

---
+++ index.js
@@ -0,0 +1,73 @@
+// TODO: Include packages needed for this application
+const path = require("path");
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: "input",
+        message: "what is the title of your project?",
+        name: "title",
+        validate: (value) => {if(value){return true}else{return 'I need a value to continure'}}, 
+    },
+    {
+        type: "input",
+        message: "what is your Github username?",
+        name: "github",
+        validate: (value) => {if(value){return true}else{return 'I need a value to continure'}}, 
+    },
+    {
+        type: "input",
+        message: "what is your email?",
+        name: "email",
+        validate: (value) => {if(value){return true}else{return 'I need a value to continure'}}, 
+    },
+    {
+        type: "input",
+        message: "Please give a description of your project",
+        name: "description",
+        validate: (value) => {if(value){return true}else{return 'I need a value to continure'}}, 
+    },
+    {
+        type: "list",
+        message: "What type of license do you have for your project?",
+        name: "license",
+        choices: ['MIT', 'Apache', 'GPL', 'None'],
+    },
+    {
+        type: "input",
+        message: "What are the steps required to build your project?",
+        name: "installation",
+        validate: (value) => {if(value){return true}else{return 'I need a value to continure'}}, 
+    },
+    {
+        type: "input",
+        message: "Please provide instructions on how to use your project",
+        name: "usage",
+        validate: (value) => {if(value){return true}else{return 'I need a value to continure'}}, 
+    },
+    {
+        type: "input",
+        message: "Please include guidelines on how other users can contribute to your project",
+        name: "contributing",
+        validate: (value) => {if(value){return true}else{return 'I need a value to continure'}}, 
+    },
+    {
+        type: "input",
+        message: "What command should be run to test the project",
+        name: "tests",
+        validate: (value) => {if(value){return true}else{return 'I need a value to continure'}}, 
+    },
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
