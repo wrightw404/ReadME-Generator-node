135d3c8465d9e84586887f2b8e9459000fcc3d34
Will <wrightw404@gmail.com>
2022-09-22 15:55:01 -0400

    finally figured out formatting for the READMEsample

--- READMEsample.md
+++ READMEsample.md
@@ -1,33 +1,33 @@
  # w
 
-        # Table of Contents 
-        * [Description](#description) 
-        * [Installation](#installation)
-        * [Usage](#usage)
-        * [License](#license)
-        * [Contributing](#contributing)
-        * [Tests](#tests)
-        * [Questions](#questions)
+# Table of Contents 
+* [Description](#description) 
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
         
-        # Description 
-        # w
+# Description 
+# w
 
-        # installation
-        # w
+# installation
+# w
 
-        # usage 
-        # w
+# usage 
+# w
 
-        # license 
-        # ![badge](https://img.shields.io/badge/license-Apache_2.0-brightgreen)
-        # This project is using a(n) Apache_2.0 license.
+# license 
+# ![badge](https://img.shields.io/badge/license-Apache_2.0-brightgreen)
+# This project is using a(n) Apache_2.0 license.
 
-        # contributing 
-        # w
+# contributing 
+# w
 
-        # tests
-        # w
+# tests
+# w
 
-        # Questions
-        # Find me on GitHub: (https://github.com/w)
-        # If you have any questions or concerns you can reach out to me by email at [w](mailto:w)
+# Questions
+# Find me on GitHub: (https://github.com/w)
+# If you have any questions or concerns you can reach out to me by email at [w](mailto:w)
--- generateReadme.js
+++ generateReadme.js
@@ -1,35 +1,36 @@
  export default function generateReadme(data) {
-    return` # ${data.title}
+    return ` # ${data.title}
 
-        # Table of Contents 
-        * [Description](#description) 
-        * [Installation](#installation)
-        * [Usage](#usage)
-        * [License](#license)
-        * [Contributing](#contributing)
-        * [Tests](#tests)
-        * [Questions](#questions)
+# Table of Contents 
+* [Description](#description) 
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
         
-        # Description 
-        # ${data.description}
+# Description 
+# ${data.description}
 
-        # installation
-        # ${data.installation}
+# installation
+# ${data.installation}
 
-        # usage 
-        # ${data.usage}
+# usage 
+# ${data.usage}
 
-        # license 
-        # ![badge](https://img.shields.io/badge/license-${data.license}-brightgreen)
-        # This project is using a(n) ${data.license} license.
+# license 
+# ![badge](https://img.shields.io/badge/license-${data.license}-brightgreen)
+# This project is using a(n) ${data.license} license.
 
-        # contributing 
-        # ${data.contributing}
+# contributing 
+# ${data.contributing}
 
-        # tests
-        # ${data.tests}
+# tests
+# ${data.tests}
 
-        # Questions
-        # Find me on GitHub: (https://github.com/${data.github})
-        # If you have any questions or concerns you can reach out to me by email at [${data.email}](mailto:${data.email})`;
+# Questions
+# Find me on GitHub: (https://github.com/${data.github})
+# If you have any questions or concerns you can reach out to me by email at [${data.email}](mailto:${data.email})
+`;
 }
